44a88b7e37e0064ae872877bfe29641ec4dcead8
giu0213@naver.com <giu0213@naver.com>
2021-10-26 13:41:44 +0900

    [ADD] register page ui

--- src/component/RegisterPage/RegisterPage.js
+++ src/component/RegisterPage/RegisterPage.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 function RegisterPage() {
-  return <div>RegisterPage</div>;
+  return (
+    <div className="auth-wrapper">
+      <h3>Register</h3>
+
+      <form>
+        <label>Email</label>
+        <input name="email" type="email" />
+
+        <label>Name</label>
+        <input name="name" />
+
+        <label>Password</label>
+        <input name="password" type="password" />
+
+        <label>Password Confirm</label>
+        <input name="passwordConfirm" type="password" />
+
+        <input type="submit" />
+
+        <Link style={{ color: "gray", textDecoration: "none" }} to="/login">
+          이미 아이디가 있다면...
+        </Link>
+      </form>
+    </div>
+  );
 }
 
 export default RegisterPage;
