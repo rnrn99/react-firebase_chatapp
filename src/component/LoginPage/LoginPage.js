a7bc4e40755db07120d773db7aa406ea23749251
giu0213@naver.com <giu0213@naver.com>
2021-10-29 15:58:26 +0900

    [ADD] login page layout & sign-in

--- src/component/LoginPage/LoginPage.js
+++ src/component/LoginPage/LoginPage.js
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import firebase from "../../firebase";
 
 function LoginPage() {
-  return <div>LoginPage</div>;
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const [SubmitError, setSubmitError] = useState("");
+  const [Loading, setLoading] = useState(false);
+
+  const onSubmit = async (data) => {
+    try {
+      setLoading(true);
+
+      await firebase
+        .auth()
+        .signInWithEmailAndPassword(data.email, data.password);
+
+      setLoading(false);
+    } catch (error) {
+      setSubmitError(error.message);
+      setLoading(false);
+      setTimeout(() => setSubmitError(""), 5000);
+    }
+  };
+
+  return (
+    <div className="auth-wrapper">
+      <h3>Login</h3>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>Email</label>
+        <input
+          name="email"
+          type="email"
+          {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
+        />
+        {errors.email && <p>이메일을 입력해 주세요</p>}
+
+        <label>Password</label>
+        <input
+          name="password"
+          type="password"
+          {...register("password", { required: true, minLength: 6 })}
+        />
+        {errors.password && errors.password.type === "required" && (
+          <p>비밀번호를 입력해 주세요</p>
+        )}
+        {errors.password && errors.password.type === "minLength" && (
+          <p>비밀번호는 6자 이상만 가능합니다</p>
+        )}
+
+        {SubmitError && <p>{SubmitError}</p>}
+        <input type="submit" disabled={Loading} />
+
+        <Link style={{ color: "gray", textDecoration: "none" }} to="/register">
+          아직 아이디가 없다면...
+        </Link>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
