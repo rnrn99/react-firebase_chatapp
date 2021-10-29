bb74e85da56b96c8afe5859a77dd87e284271a65
giu0213@naver.com <giu0213@naver.com>
2021-10-29 17:02:58 +0900

    [ADD] save user info in the redux store

--- src/App.js
+++ src/App.js
@@ -1,29 +1,77 @@
 import React, { useEffect } from "react";
 import { Switch, Route, useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import firebase from "./firebase";
+import { setUser } from "./redux/action/userAction";
+
 import LoginPage from "./component/LoginPage/LoginPage";
 import RegisterPage from "./component/RegisterPage/RegisterPage";
 import ChatPage from "./component/ChatPage/ChatPage";
 
 function App() {
   let history = useHistory();
+  let dispatch = useDispatch();
+  const isLoading = useSelector((state) => state.user.isLoading);
+
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         history.push("/");
+        dispatch(setUser(user));
       } else {
         history.push("/login");
       }
     });
   }, [history]);
 
-  return (
-    <Switch>
-      <Route exact path="/" component={ChatPage} />
-      <Route exact path="/login" component={LoginPage} />
-      <Route exact path="/register" component={RegisterPage} />
-    </Switch>
-  );
+  if (isLoading) {
+    return (
+      <div style={{ position: "absolute", left: "50%", top: "50%" }}>
+        <svg
+          version="1.1"
+          id="loader-1"
+          xmlns="http://www.w3.org/2000/svg"
+          x="0px"
+          y="0px"
+          width="40px"
+          height="40px"
+          viewBox="0 0 40 40"
+          enable-background="new 0 0 40 40"
+        >
+          <path
+            opacity="0.2"
+            fill="#000"
+            d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946
+    s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z M20.201,31.749c-6.425,0-11.634-5.208-11.634-11.634
+    c0-6.425,5.209-11.634,11.634-11.634c6.425,0,11.633,5.209,11.633,11.634C31.834,26.541,26.626,31.749,20.201,31.749z"
+          />
+          <path
+            fill="#000"
+            d="M26.013,10.047l1.654-2.866c-2.198-1.272-4.743-2.012-7.466-2.012h0v3.312h0
+    C22.32,8.481,24.301,9.057,26.013,10.047z"
+          >
+            <animateTransform
+              attributeType="xml"
+              attributeName="transform"
+              type="rotate"
+              from="0 20 20"
+              to="360 20 20"
+              dur="0.5s"
+              repeatCount="indefinite"
+            />
+          </path>
+        </svg>
+      </div>
+    );
+  } else {
+    return (
+      <Switch>
+        <Route exact path="/" component={ChatPage} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/register" component={RegisterPage} />
+      </Switch>
+    );
+  }
 }
 
 export default App;
--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from "redux";
+import user from "./userReducer";
 
 const rootReducer = combineReducers({
-  // reducer
+  user,
 });
 
 export default rootReducer;
