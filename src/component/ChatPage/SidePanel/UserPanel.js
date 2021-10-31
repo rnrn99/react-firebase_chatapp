8ed7af4d511bb9403f8e6ee9ae3417913f36c002
giu0213@naver.com <giu0213@naver.com>
2021-10-31 20:11:58 +0900

    [ADD] user panel UI

--- src/component/ChatPage/SidePanel/UserPanel.js
+++ src/component/ChatPage/SidePanel/UserPanel.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { IoMdChatboxes } from "react-icons/io";
+import { Dropdown, Image } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 function UserPanel() {
-  return <div>UserPanel</div>;
+  const user = useSelector((state) => state.user.currentUser);
+
+  return (
+    <div>
+      <h3 style={{ color: "white", fontWeight: "bold" }}>
+        <IoMdChatboxes /> Chat App
+      </h3>
+
+      <div style={{ display: "flex", marginBottom: "1rem" }}>
+        <Image
+          src={user && user.photoURL}
+          style={{ width: "30px", height: "30px", marginTop: "3px" }}
+          roundedCircle
+        />
+        <Dropdown>
+          <Dropdown.Toggle
+            id="dropdown-basic"
+            style={{ background: "transparent", border: "0px" }}
+          >
+            {user && user.displayName}
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item onClick>프로필 사진 변경</Dropdown.Item>
+            <Dropdown.Item onClick>로그아웃</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+    </div>
+  );
 }
 
 export default UserPanel;
