e291dc7c8a0c928a65007ce0114565f37c8a53c1
giu0213@naver.com <giu0213@naver.com>
2021-11-10 15:30:04 +0900

    [ADD] message form UI

--- src/component/ChatPage/MainPanel/MessageForm.js
+++ src/component/ChatPage/MainPanel/MessageForm.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { Form, ProgressBar, Row, Col } from "react-bootstrap";
 
 function MessageForm() {
-  return <div>MessageForm</div>;
+  return (
+    <div>
+      <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+      </Form>
+
+      <ProgressBar animated label="" now={60} />
+
+      <Row>
+        <Col>
+          <button className="message-form-button" style={{ width: "100%" }}>
+            SEND
+          </button>
+        </Col>
+        <Col>
+          <button className="message-form-button" style={{ width: "100%" }}>
+            UPLOAD
+          </button>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default MessageForm;
