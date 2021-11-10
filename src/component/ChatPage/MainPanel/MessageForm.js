7b283902f98ec375e213cb4e7189f43d7dfbfb82
giu0213@naver.com <giu0213@naver.com>
2021-11-10 16:14:19 +0900

    [ADD] send message

--- src/component/ChatPage/MainPanel/MessageForm.js
+++ src/component/ChatPage/MainPanel/MessageForm.js
@@ -1,12 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, ProgressBar, Row, Col } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import firebase from "../../../firebase";
 
 function MessageForm() {
+  const chatRoom = useSelector((state) => state.chatRoom.currentChatRoom);
+  const user = useSelector((state) => state.user.currentUser);
+
+  const [Content, setContent] = useState("");
+  const [Loading, setLoading] = useState(false);
+  const messageRef = firebase.database().ref("message");
+
+  const handleChange = (e) => {
+    setContent(e.target.value);
+  };
+
+  const createMessage = (fileUrl = null) => {
+    const message = {
+      timestamp: firebase.database.ServerValue.TIMESTAMP,
+      user: {
+        id: user.uid,
+        name: user.displayName,
+        image: user.photoURL,
+      },
+    };
+
+    if (fileUrl !== null) {
+      // 파일 전송
+      message["image"] = fileUrl;
+    } else {
+      // 메세지 전송
+      message["content"] = Content;
+    }
+
+    return message;
+  };
+  const handleSubmit = async () => {
+    if (!Content) {
+      alert("Type contents first");
+      return;
+    }
+    setLoading(true);
+
+    try {
+      await messageRef.child(chatRoom.id).push().set(createMessage());
+      setLoading(false);
+      setContent("");
+    } catch (error) {
+      alert(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <div>
-      <Form>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Control as="textarea" rows={3} />
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Control
+            as="textarea"
+            rows={3}
+            value={Content}
+            onChange={handleChange}
+          />
         </Form.Group>
       </Form>
 
@@ -15,12 +70,16 @@ function MessageForm() {
       <Row>
         <Col>
           <button className="message-form-button" style={{ width: "100%" }}>
-            SEND
+            UPLOAD
           </button>
         </Col>
         <Col>
-          <button className="message-form-button" style={{ width: "100%" }}>
-            UPLOAD
+          <button
+            className="message-form-button"
+            style={{ width: "100%" }}
+            onClick={handleSubmit}
+          >
+            SEND
           </button>
         </Col>
       </Row>
