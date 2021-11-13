02610196d282ca1e049716d95385dacc08d9ab24
giu0213@naver.com <giu0213@naver.com>
2021-11-14 07:59:12 +0900

    [ADD] get a list of users for DM

--- src/component/ChatPage/SidePanel/DirectMessage.js
+++ src/component/ChatPage/SidePanel/DirectMessage.js
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
+import { AiOutlineSmile } from "react-icons/ai";
+import { connect } from "react-redux";
+import firebase from "../../../firebase";
 
-function DirectMessage() {
-  return <div>DirectMessage</div>;
+export class DirectMessage extends Component {
+  state = {
+    userRef: firebase.database().ref("user"),
+    user: [],
+  };
+
+  componentDidMount() {
+    if (this.props.user) {
+      this.addUserListener();
+    }
+  }
+
+  addUserListener = () => {
+    let arrUser = [];
+    this.state.userRef.on("child_added", (snapshot) => {
+      if (this.props.user.uid !== snapshot.key) {
+        let userInfo = snapshot.val();
+        userInfo["uid"] = snapshot.key;
+        userInfo["status"] = "offline";
+        arrUser.push(userInfo);
+        this.setState({ user: arrUser });
+      }
+    });
+  };
+
+  renderDM = () => {};
+
+  render() {
+    return (
+      <div>
+        <span style={{ display: "flex", alignItems: "center" }}>
+          <AiOutlineSmile style={{ marginRight: 3 }} /> DM ({})
+        </span>
+
+        <ul style={{ listStyleType: "none", padding: 0 }}>{this.renderDM()}</ul>
+      </div>
+    );
+  }
 }
 
-export default DirectMessage;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user.currentUser,
+  };
+};
+
+export default connect(mapStateToProps)(DirectMessage);
