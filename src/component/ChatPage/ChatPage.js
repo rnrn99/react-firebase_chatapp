c3ac3d5b34c6356735d954bbcd1e6a5b061ec39a
giu0213@naver.com <giu0213@naver.com>
2021-11-17 15:24:05 +0900

    [MOD] correcting the loading error of DM's user list

--- src/component/ChatPage/ChatPage.js
+++ src/component/ChatPage/ChatPage.js
@@ -4,11 +4,12 @@ import MainPanel from "./MainPanel/MainPanel";
 import SidePanel from "./SidePanel/SidePanel";
 
 function ChatPage() {
+  const user = useSelector((state) => state.user.currentUser);
   const chatRoom = useSelector((state) => state.chatRoom.currentChatRoom);
   return (
     <div style={{ display: "flex" }}>
       <div style={{ width: "300px" }}>
-        <SidePanel />
+        <SidePanel key={user && user.uid} />
       </div>
       <div style={{ width: "100%" }}>
         <MainPanel key={chatRoom && chatRoom.id} />
